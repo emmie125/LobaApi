c4c5cd91bee93d722e002e97a3ca2d7beb02ea5c
emmie125 <mulangaemmanuella@gmail.com>
2021-12-15 12:10:01 +0100

    fix: fixa bug method attempt in login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -69,7 +69,7 @@ class AuthController extends Controller
         $tokenResult = $user->createToken('Personal Access Token');
         $token = $tokenResult->token;
         if ($request->remember_me)
-            $token->expires_at = Carbon::now()->addWeeks(1);
+            $token->expires_at = Carbon::now()->addDays(3);
         $token->save();
         return response()->json([
             'access_token' => $tokenResult->accessToken,
