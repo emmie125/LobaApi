3a7d73fb6a42ab9f5b741ef37b289ddcae0b79b5
emmie125 <mulangaemmanuella@gmail.com>
2022-01-05 16:59:59 +0100

    refactor: refactor code

---
+++ app/Http/Controllers/AlerteController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Twilio\Rest\Client;
+use Illuminate\Http\Request;
+use Illuminate\Contracts\Encryption\DecryptException;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Crypt;
+
+class AlerteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function create_alert(Request $request)
+    {
+        $sid = "AC6e38e8372b28634c54b253a9fe871a68";
+        $token = "1e8c8decdb15c04336821488eb43ae89";
+        $twilio = new Client($sid, $token);
+        $encryptedValue = Auth::accessToken();
+        $decrypted = Crypt::decryptString($encryptedValue);
+        $message = $twilio->messages
+            ->create(
+                "+243826370009", // to
+                ["body" => "Hello c'est Loba", "from" => "+15739733908"]
+            );
+        print($message->sid);
+        //return response()->json($request->user()->id);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Models/TypeAlert.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TypeAlert extends Model
+{
+
+    protected $fillable = ['type'];
+}
